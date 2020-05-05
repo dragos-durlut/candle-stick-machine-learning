987925235089c6bd556cff8d21d85ced03e781d0
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-05-05 18:27:49 +0300

    - added Trainer

--- CandleStickMachineLearningConsole/Program.cs
+++ CandleStickMachineLearningConsole/Program.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
+using Microsoft.ML;
+using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
 using System;
 using System.Threading.Tasks;
 
@@ -33,6 +36,41 @@ namespace CandleStickMachineLearning
             //do the actual work here
             var yahooFinanceService = serviceProvider.GetService<IYahooFinanceService>();
             var barsList = await yahooFinanceService.GetBars("AAPL", DateTime.UtcNow.AddMonths(-1), DateTime.UtcNow, "1h");
+
+            var context = new MLContext();
+
+            var trainData = context.Data.LoadFromEnumerable<Models.Bar>(barsList);
+
+            var settings = new RegressionExperimentSettings
+            {
+                MaxExperimentTimeInSeconds = 20,
+                OptimizingMetric = RegressionMetric.MeanAbsoluteError
+            };
+
+            var labelColumnInfo = new ColumnInformation()
+            {
+                LabelColumnName = "Label"
+            };
+
+            var progress = new Progress<RunDetail<RegressionMetrics>>(p =>
+            {
+                if (p.ValidationMetrics != null)
+                {
+                    _logger.LogInformation($"Current Result - {p.TrainerName}, {p.ValidationMetrics.RSquared}, {p.ValidationMetrics.MeanAbsoluteError}");
+                }
+            });
+
+            var experiment = context.Auto().CreateRegressionExperiment(settings);
+
+            var result = experiment.Execute(trainData, labelColumnInfo, progressHandler: progress);
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Best run:");
+            Console.WriteLine($"Trainer name - {result.BestRun.TrainerName}");
+            Console.WriteLine($"RSquared - {result.BestRun.ValidationMetrics.RSquared}");
+            Console.WriteLine($"MAE - {result.BestRun.ValidationMetrics.MeanAbsoluteError}");
+
+            Console.ReadLine();
         }
     }
 }
